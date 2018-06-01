bf216f4687e55bfe9870e45a1709598d0330bfa6
Daniel Kaiser <daniel.kaiser94@gmail.com>
2018-06-01 11:00:31 +0200

    Update to version 2.39

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ class DownloadChromedriver(build_py):
 
 setup(
     name="chromedriver-binary",
-    version="2.38.0",
+    version="2.39.0",
     author="Daniel Kaiser",
     author_email="daniel.kaiser94@gmail.com",
     description="Installer for chromedriver.",
