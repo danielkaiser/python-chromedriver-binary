e1d11223a1dda1a82c5d4ea7ebd50b68f1c19bb1
Daniel Kaiser <daniel.kaiser94@gmail.com>
2018-10-17 21:28:29 +0200

    Update to version 2.41

--- chromedriver_binary/utils.py
+++ chromedriver_binary/utils.py
@@ -29,11 +29,11 @@ def get_variable_separator():
     return ':'
 
 
-def get_chromedriver_url(version='2.42'):
+def get_chromedriver_url(version='2.43'):
     """
-    Generates the download URL for current platform , architecture and the given version. Default version is 2.42.
+    Generates the download URL for current platform , architecture and the given version. Default version is 2.43.
     Supports Linux, MacOS and Windows.
-    :param version: chromedriver version string, default '2.42'
+    :param version: chromedriver version string, default '2.43'
     :return: Download URL for chromedriver
     """
     base_url = 'https://chromedriver.storage.googleapis.com/'
