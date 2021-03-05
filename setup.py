8934395a908b19ad90ef0148348c9073abae63c6
Daniel Kaiser <daniel.kaiser94@gmail.com>
2021-03-05 10:08:15 +0100

    Update PyPI version for python-chromedriver-auto

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ class DownloadChromedriver(build_py):
 
 setup(
     name="chromedriver-binary-auto",
-    version="0.1",
+    version="0.1.1",
     author="Daniel Kaiser",
     author_email="daniel.kaiser94@gmail.com",
     description="Installer for chromedriver.",
