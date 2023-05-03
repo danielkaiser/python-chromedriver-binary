2f8dd9976baa92941904c615bdbc9f187f375b84
Daniel Kaiser <d.kaiser@fz-juelich.de>
2023-05-03 15:04:36 +0200

    Release 0.2.6

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ class DownloadChromedriver(build_py):
 
 setup(
     name="chromedriver-binary-auto",
-    version="0.2.5",
+    version="0.2.6",
     author="Daniel Kaiser",
     author_email="daniel.kaiser94@gmail.com",
     description="Installer for chromedriver.",
