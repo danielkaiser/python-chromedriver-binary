5ee16977cfe617e30486cc5bf112342cb3b2b33f
Daniel Kaiser <d.kaiser@fz-juelich.de>
2026-02-22 04:03:35 +0000

    Chromedriver version 147.0.7699.0

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ class DownloadChromedriver(build_py):
         Downloads, unzips and installs chromedriver.
         If a chromedriver binary is found in PATH it will be copied, otherwise downloaded.
         """
-        chromedriver_version='147.0.7698.0'
+        chromedriver_version='147.0.7699.0'
         chromedriver_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'chromedriver_binary')
         chromedriver_filename = find_binary_in_path(get_chromedriver_filename())
         if chromedriver_filename and check_version(chromedriver_filename, chromedriver_version):
@@ -65,7 +65,7 @@ class DownloadChromedriver(build_py):
 
 setup(
     name="chromedriver-binary",
-    version="147.0.7698.0.0",
+    version="147.0.7699.0.0",
     author="Daniel Kaiser",
     author_email="daniel.kaiser94@gmail.com",
     description="Installer for chromedriver.",
